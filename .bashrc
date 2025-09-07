8164898e9f25cb4becc6efc0b38bed8c7a3d6426
slipperynick <nick@badclick.org>
2025-09-07 17:34:47 +0200

    updates to aliases and zshrc

--- .bashrc
+++ .bashrc
@@ -1,5 +1,8 @@
 # .bashrc
 # Nicholas Harripersad
+# TODO
+# - Add git branch to prompt
+# - Cleanup old items
 
 
 #PS1="\[\033[35m\]\t\[\033[m\]-\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,13 @@
+# Aliases for zsh and macOS
+
 alias mp="multipass"
 alias k="kubecolor"
 alias ls="eza"
+alias ll='ls -la'
+alias c='clear'
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
